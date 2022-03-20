ff4d41642994ea50088cd662d25bfe4903c3df81
carloo07 <76703454+carloo07@users.noreply.github.com>
2022-03-20 03:52:23 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -25,9 +25,7 @@ module.exports = {
     if (!song) {
       setTimeout(function () {
         if (queue.connection.dispatcher && message.guild.me.voice.channel) return;
-        queue.textChannel.send(i18n.__("play.leaveChannel"));
-      }, STAY_TIME * 1000);
-      queue.textChannel.send(i18n.__("play.queueEnded")).catch(console.error);
+       },
       return message.client.queue.delete(message.guild.id);
     }
 
